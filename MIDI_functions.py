3af1f144fd827e665111dc0ff6445fea7be7e657
hernanmelmot <hdperezn@unal.edu.co>
2023-03-01 11:28:53 -0500

    first midi functions

---
+++ MIDI_functions.py
@@ -0,0 +1,54 @@
+import collections
+import fluidsynth
+import glob
+import numpy as np
+import pathlib
+import pandas as pd
+import pretty_midi
+import seaborn as sns
+
+from IPython import display
+from matplotlib import pyplot as plt
+from typing import Dict, List, Optional, Sequence, Tuple
+
+def display_audio(pm: pretty_midi.PrettyMIDI, seconds=60):
+  waveform = pm.fluidsynth(fs=_SAMPLING_RATE)
+  # Take a sample of the generated waveform to mitigate kernel resets
+  waveform_short = waveform[:seconds*_SAMPLING_RATE]
+  return display.Audio(waveform_short, rate=_SAMPLING_RATE)
+
+def midi_to_notes(midi_file: str) -> pd.DataFrame:
+  pm = pretty_midi.PrettyMIDI(midi_file)
+  instrument = pm.instruments[0]
+  notes = collections.defaultdict(list)
+
+  # Sort the notes by start time
+  sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
+  prev_start = sorted_notes[0].start
+
+  for note in sorted_notes:
+    start = note.start
+    end = note.end
+    notes['pitch'].append(note.pitch)
+    notes['start'].append(start)
+    notes['end'].append(end)
+    notes['step'].append(start - prev_start)
+    notes['duration'].append(end - start)
+    prev_start = start
+
+  return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+
+def plot_piano_roll(notes: pd.DataFrame, count: Optional[int] = None):
+  if count:
+    title = f'First {count} notes'
+  else:
+    title = f'Whole track'
+    count = len(notes['pitch'])
+  plt.figure(figsize=(20, 4))
+  plot_pitch = np.stack([notes['pitch'], notes['pitch']], axis=0)
+  plot_start_stop = np.stack([notes['start'], notes['end']], axis=0)
+  plt.plot(
+      plot_start_stop[:, :count], plot_pitch[:, :count], color="b", marker=".")
+  plt.xlabel('Time [s]')
+  plt.ylabel('Pitch')
+  _ = plt.title(title)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # MIDI_functions
-some important functions to process MIDI
+some important functions to process MIDI in order to use them in Deep learning Aplication
+
+
+# Dependencies
+
+*   fluidsynth
+*   pyfluidsynth
+*   pretty_midi
+
+# Functions: 
+
